7fc4f5a6530ac70d8a1673316143243645166432
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-03 13:55:26 +0100

    Setup App.js with class component App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
-import "./App.css";
 
-function App() {
-  return <div className="App" />;
+class App extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="App">
+        <h1>Meeting</h1>
+      </div>
+    );
+  }
 }
 
 export default App;
