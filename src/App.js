1709948f1208be7053ff79ff93192f7bfab1a2db
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-04 17:32:47 +0100

    add lead lorum text to App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,15 @@ class App extends Component {
       <div className="container text-center">
         <div className="row justify-content-center">
           <h1>Meeting</h1>
+          <p className="lead">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+            commodo a justo vitae aliquet. Vivamus erat justo, eleifend a sem
+            non, elementum viverra lectus. Morbi eu quam sapien. Pellentesque
+            consequat lectus id erat auctor tincidunt. Cras maximus lacinia
+            aliquam. Vestibulum posuere lorem vitae cursus tincidunt. Duis ut
+            efficitur orci. Maecenas tincidunt lacus diam, id molestie nisl
+            vestibulum sed.
+          </p>
         </div>
       </div>
     );
