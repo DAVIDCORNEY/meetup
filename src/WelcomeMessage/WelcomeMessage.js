0bdffe6d354a4729cbde9bd2b770e7ef328f922c
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-04 19:26:30 +0100

    Add logic to render WelcomeMessage conditionally

--- src/WelcomeMessage/WelcomeMessage.js
+++ src/WelcomeMessage/WelcomeMessage.js
@@ -2,7 +2,17 @@ import React, { Component } from "react";
 
 class WelcomeMessage extends Component {
   render() {
-    return <div />;
+    const { user } = this.props;
+    return (
+      <div className="text-center mt-4">
+        <span className="text-secondary font-weight-bold pl-1">
+          Welcome {user},
+        </span>
+        <a href="/" className="font-weight-bold text-primary pl-1">
+          log out
+        </a>
+      </div>
+    );
   }
 }
 
