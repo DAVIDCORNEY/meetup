d7562a2bb8d40d63c6e49e42ce6baca4848e2eed
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-04 17:40:31 +0100

    Add Register button App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,21 @@ class App extends Component {
     return (
       <div className="container text-center">
         <div className="row justify-content-center">
-          <h1>Meeting</h1>
-          <p className="lead">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-            commodo a justo vitae aliquet. Vivamus erat justo, eleifend a sem
-            non, elementum viverra lectus. Morbi eu quam sapien. Pellentesque
-            consequat lectus id erat auctor tincidunt. Cras maximus lacinia
-            aliquam. Vestibulum posuere lorem vitae cursus tincidunt. Duis ut
-            efficitur orci. Maecenas tincidunt lacus diam, id molestie nisl
-            vestibulum sed.
-          </p>
+          <div className="col-10 col-md-10 col-lg-10">
+            <h1>Meeting</h1>
+            <p className="lead">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              commodo a justo vitae aliquet. Vivamus erat justo, eleifend a sem
+              non, elementum viverra lectus. Morbi eu quam sapien. Pellentesque
+              consequat lectus id erat auctor tincidunt. Cras maximus lacinia
+              aliquam. Vestibulum posuere lorem vitae cursus tincidunt. Duis ut
+              efficitur orci. Maecenas tincidunt lacus diam, id molestie nisl
+              vestibulum sed.
+            </p>
+            <a href="/register" className="btn btn-outline-primary mr-2">
+              Register
+            </a>
+          </div>
         </div>
       </div>
     );
