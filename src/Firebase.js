372241298e911fb890f3ef7bd1410c0e917e21b2
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-06 12:33:44 +0100

    Setup firebase config file Firebase.js

---
+++ src/Firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/database";
+import "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBcseQuxzRKLShHBilP_l8L85tVBBNfJxY",
+  authDomain: "meetup-1dd6b.firebaseapp.com",
+  databaseURL: "https://meetup-1dd6b.firebaseio.com",
+  projectId: "meetup-1dd6b",
+  storageBucket: "",
+  messagingSenderId: "920077160604",
+  appId: "1:920077160604:web:533a96f18600a97f"
+};
+firebase.initializeApp(firebaseConfig);
