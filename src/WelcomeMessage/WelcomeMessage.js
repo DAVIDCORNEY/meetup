6e09ad6b246e6cd3f696271cec92886030fd8aa4
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-07 16:05:22 +0100

    change user props to userName in WelcomeMessage

--- src/WelcomeMessage/WelcomeMessage.js
+++ src/WelcomeMessage/WelcomeMessage.js
@@ -3,11 +3,11 @@ import { Link } from "@reach/router";
 
 class WelcomeMessage extends Component {
   render() {
-    const { user } = this.props;
+    const { userName } = this.props;
     return (
       <div className="text-center mt-4">
         <span className="text-secondary font-weight-bold pl-1">
-          Welcome {user},
+          Welcome {userName},
         </span>
         <Link to="/login" className="font-weight-bold text-primary pl-1">
           log out
