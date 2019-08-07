ea7284abe67e7a540e787a3772a7b6d4c4ed5c14
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-07 18:24:03 +0100

    Add login form to Login component

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -3,9 +3,55 @@ import React, { Component } from "react";
 class Login extends Component {
   render() {
     return (
-      <div className="text-center mt-4">
-        <h1 className="text-primary">Login</h1>
-      </div>
+      <form className="mt-3">
+        <div className="container">
+          <div className="row justify content-center">
+            <div className="col-lg-6">
+              <div className="card bg-light">
+                <div className="card-body">
+                  <h3 className="font-weight-light mb3">Log in</h3>
+                  <section className="form-group">
+                    <label
+                      className="form-control-label sr-only"
+                      htmlFor="Email"
+                    >
+                      Email
+                    </label>
+                    <input
+                      className="form-control"
+                      type="email"
+                      id="email"
+                      placeholder="Email"
+                      name="email"
+                      required
+                    />
+                  </section>
+                  <section className="form-group">
+                    <label
+                      className="form-control-label sr-only"
+                      htmlFor="Password"
+                    >
+                      Password
+                    </label>
+                    <input
+                      className="form-control"
+                      type="password"
+                      placeholder="Password"
+                      required
+                      name="password"
+                    />
+                  </section>
+                  <div className="form-group text-right mb-0">
+                    <button className="btn btn-primary" type="submit">
+                      Log in
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </form>
     );
   }
 }
