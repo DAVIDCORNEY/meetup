33b4397162cc5b2272a8db7faf25965307a3a966
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-07 17:52:28 +0100

    add onClick method to WelcomeMessage accepting logoutUser

--- src/WelcomeMessage/WelcomeMessage.js
+++ src/WelcomeMessage/WelcomeMessage.js
@@ -3,13 +3,17 @@ import { Link } from "@reach/router";
 
 class WelcomeMessage extends Component {
   render() {
-    const { userName } = this.props;
+    const { userName, logoutUser } = this.props;
     return (
       <div className="text-center mt-4">
         <span className="text-secondary font-weight-bold pl-1">
           Welcome {userName},
         </span>
-        <Link to="/login" className="font-weight-bold text-primary pl-1">
+        <Link
+          to="/login"
+          className="font-weight-bold text-primary pl-1"
+          onClick={event => logoutUser(event)}
+        >
           log out
         </Link>
       </div>
