14ec313beb8a6285ed35744599e1211a84719fc8
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-08 15:06:40 +0100

    Add delete button to MeetingList

--- src/MeetingsList/MeetingsList.js
+++ src/MeetingsList/MeetingsList.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-import { Link } from "@reach/router";
 
 class MeetingsList extends Component {
   render() {
@@ -7,6 +6,19 @@ class MeetingsList extends Component {
     const myMeetings = meetings.map(item => {
       return (
         <div className="list-group-item d-flex" key={item.meetingID}>
+          <section
+            className="btn-group align-self-center"
+            role="group"
+            aria-label="Meeting Information"
+          >
+            <button
+              className="btn btn-sm btn-outline-secondary"
+              title="Delete Meeting"
+            >
+              Button
+            </button>
+          </section>
+
           <section className="pl-3 text-left align-self-center">
             {item.meetingName}
           </section>
