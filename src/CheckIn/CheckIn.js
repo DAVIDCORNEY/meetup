d19c495dcd1392acf658924211cc4b3e4997c17b
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-08 16:15:50 +0100

    CheckIn import firebase

---
+++ src/CheckIn/CheckIn.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import firebase from "../Firebase";
+
+class CheckIn extends Component {
+  state = {};
+  render() {
+    return <div />;
+  }
+}
+
+export default CheckIn;
