3c624bd1818a8ee0cd0bd9d5feb2a33dbab1940d
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-09 17:27:37 +0100

    Attendees list add logic to render list of attendees

--- src/AttendeesList/AttendeesList.js
+++ src/AttendeesList/AttendeesList.js
@@ -1,7 +1,21 @@
 import React from "react";
 
-const AttendeesList = () => {
-  return <div>Attendees</div>;
+const AttendeesList = ({ attendees }) => {
+  const allAttendees = attendees.map(attendee => {
+    return (
+      <div
+        className="col-8 col-sm-6 col-md-4 col-lg-3 mb-2 p-0 px-1"
+        key={attendee.attendeeID}
+      >
+        <div className="card">
+          <div className="card-body px-3 py-2 d-flex align-items-center justify-content-center">
+            <div>{attendee.attendeeName}</div>
+          </div>
+        </div>
+      </div>
+    );
+  });
+  return <div className="row justify-content-center">{allAttendees}</div>;
 };
 
 export default AttendeesList;
