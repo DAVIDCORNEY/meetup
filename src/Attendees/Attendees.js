404f64e909e6c57eba27d684014e74c34ce841c1
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-09 16:18:33 +0100

    Attendees add container and h1

--- src/Attendees/Attendees.js
+++ src/Attendees/Attendees.js
@@ -6,7 +6,16 @@ class Attendees extends Component {
     attendees: []
   };
   render() {
-    return <div />;
+    return (
+      <div className="container mt-4">
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            <h1 className="font-weight-light text-center">Attendees</h1>
+          </div>
+        </div>
+        List
+      </div>
+    );
   }
 }
 
