f4af361fbaa7df3cf617db8a6acbc50db886cd02
David Corney <16760636+DAVIDCORNEY@users.noreply.github.com>
2019-08-09 16:36:53 +0100

    Attendees build attendeesList array in CDM

--- src/Attendees/Attendees.js
+++ src/Attendees/Attendees.js
@@ -10,6 +10,17 @@ class Attendees extends Component {
     const ref = firebase
       .database()
       .ref(`meetings/${this.props.userID}/${this.props.meetingID}/attendees`);
+    ref.on("value", snapshot => {
+      let attendees = snapshot.val();
+      let attendeesList = [];
+      for (let item in attendees) {
+        attendeesList.push({
+          attendeeID: item,
+          attendeeName: attendees[item].attendeeName,
+          attendeeEmail: attendees[item].attendeeEmail
+        });
+      }
+    });
   }
 
   render() {
